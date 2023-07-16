8e949d4a6c8c8caf56892c74dc3cad97073de9f0
omar_ali <omar.sayedahmed.ali@gmail.com>
2023-07-16 14:16:00 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mat3m';
+
+  imgSrc = 'https://picsum.photos/300/300';
+  currentCustomer = 'Omar Ali';
+  clickMessege!:any;
+
+  onSave(ev:Event){
+    console.log('Hello Clicked',ev);
+  }
+
+  onKeyDown(ev:Event){
+    console.log('Hello Clicked keyDown',ev);
+  }
 }

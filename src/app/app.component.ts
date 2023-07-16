b23a1678fd6d90a0c54aedfe15006fcd6af8d846
omar_ali <omar.sayedahmed.ali@gmail.com>
2023-07-16 14:54:14 +0300

    angular 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mat3m';
-
+  saved=true;
+  disabled=false;
   imgSrc = 'https://picsum.photos/300/300';
   currentCustomer = 'Omar Ali';
   clickMessege!:any;
 
   onSave(ev:Event){
     console.log('Hello Clicked',ev);
+    this.disabled=true
   }
 
   onKeyDown(ev:Event){
